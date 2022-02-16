9f351bafe445aeef4f7255f1adb7910b258cf062
Valerra45 <valerra45@gmail.com>
2022-02-16 17:43:07 +0300

    add shared kernel abstraction

---
+++ src/Server/Point.External/Point.External.Core/Domain/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using Point.SharedKernel.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point.External.Core.Domain
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public BaseEntity()
+        {
+            Created = DateTime.Now;
+        }
+
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Update { get; set; }
+    }
+}
